8a36a3eea82d0a320065d78efa485e8ffa50423f
thinkpad <thinpad@163.com>
2025-01-21 17:45:30 +0800

    upformat

--- Core/Inc/sdio.h
+++ Core/Inc/sdio.h
@@ -22,31 +22,31 @@
 #define __SDIO_H__
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
-    /* USER CODE BEGIN Includes */
+/* USER CODE BEGIN Includes */
 
-    /* USER CODE END Includes */
+/* USER CODE END Includes */
 
-    extern SD_HandleTypeDef hsd;
+extern SD_HandleTypeDef hsd;
 
-    /* USER CODE BEGIN Private defines */
+/* USER CODE BEGIN Private defines */
 
-    /* USER CODE END Private defines */
+/* USER CODE END Private defines */
 
-    void MX_SDIO_SD_Init(void);
+void MX_SDIO_SD_Init(void);
 
-    /* USER CODE BEGIN Prototypes */
+/* USER CODE BEGIN Prototypes */
     void MX_SDIO_SD_Init_Fix(void);
-    /* USER CODE END Prototypes */
+/* USER CODE END Prototypes */
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif /* __SDIO_H__ */
+
